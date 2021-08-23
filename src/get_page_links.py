82f1bcf7e3ed8108a7e3718932e2b9c5c3313df8
lslbastos <lslbastos@gmail.com>
2021-08-23 16:15:03 +0000

    adding new files

--- src/get_page_links.py
+++ src/get_page_links.py
@@ -1,7 +1,6 @@
 #### Scrapping source URL with CSV links (OpenDataSUS)
 ## Code based/adapted from 'https://pyshark.com/extract-links-from-a-web-page-using-python/' and 'https://www.geeksforgeeks.org/extract-all-the-urls-from-the-webpage-using-python/'
 
-
 ## Scrapping OpendataSUS webpages
 import requests
 from bs4 import BeautifulSoup
